5a51e6ed3d0b21880767e8fbc9d5f0519bb6b08c
Angelica Fields <angelicafields08@gmail.com>
2018-07-29 23:49:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ var dodger = document.getElementById('dodger');
 
 function moveDodgerRight() {
   var rightNumbers = dodger.style.right.replace('px', '');
-  var right = parseInt( rightNumbers, -10);
+  var right = parseInt( rightNumbers, 10);
   if (right > 0) {
-    dodger.style.right = `${right - 1}px`
+    dodger.style.left = `${left + 1}px`
   }
 }
 
